cf3457e13db5dfd036f86ead965ad748a66d2c34
Heshercat <alfiiaqa@gmail.com>
2023-10-09 22:30:07 +0300

    Initial plan

---
+++ tf-magic-animals/magic-animals.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "initial" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, magic animals!'"
+  }
+}
+
+resource "null_resource" "timestamp-trigger" {
+  triggers = {
+    value = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "echo Resource updated at: ${self.triggers.value}"
+  }
+}
+
+resource "local_file" "foo" {
+  content  = "smth!"
+  filename = "${path.module}/smth.bar"
+}
---
+++ tf-nullresouce-ip/main.tf
@@ -0,0 +1,32 @@
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8200
+      external = 8300
+      protocol = "tcp-ip"
+    }
+  ]
+}
+
+resource "null_resource" "check_ip" {
+  triggers = {
+    current_time = timestamp()
+  }
+  provisioner "local-exec" {
+    command = "curl -s https://ifconfig.me/ip > ip.txt"
+  }
+}
+
+data "local_file" "read_ip" {
+  depends_on = [null_resource.check_ip]
+  filename   = "./ip.txt"
+}
+
+output "scalr_ip" {
+  value = "Current instance IP is: ${data.local_file.read_ip.content}\nDocker ports information is: ${jsonencode(var.docker_ports)}"
+}
